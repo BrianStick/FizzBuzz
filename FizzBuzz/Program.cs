88aa22aa1855c656c22a7c6f2731a39f87527898
Brian Stickney <brianchase055@gmail.com>
2016-03-06 03:14:34 -0600

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class For
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i <= 100; i++)
+
+                Console.WriteLine(i);
+        }
+         
+            if (int % 3 == 0);
+            
+            Console.WriteLine("Fizz");
+    }
+        
+            if (if % 5 == 0)
+        
+         Console.WriteLine("Buzz');
+        };
+
+        
+    
+
